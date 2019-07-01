467f19b11051b6098c0c0a4a21e04d08213ec84c
cb jing <jingchenbo@gmail.com>
2019-07-01 21:53:11 +0800

    “在map中添加一些基本接口”

--- include/map.h
+++ include/map.h
@@ -1,8 +1,16 @@
 #ifndef __MAP__H__
 #define __MAP__H__
+
 namespace gmap {
     //Map类负责需要渲染的图片的所有信息的维护，比如地图的范围，分辨率。并且渲染图片
     class Map{
+        public:
+            Map();
+            //初始化地图
+            Map(int width, int hight);
+            void SetBounds(double xmin, double ymin, double xmax, double ymax);
+            void SetRes(double res);
+            bool Render
 
     };
 }
