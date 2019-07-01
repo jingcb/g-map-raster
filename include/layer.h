f82533698df17250c90a62183ce55b5575b3e330
cb jing <jingchenbo@gmail.com>
2019-07-01 21:14:37 +0800

    “添加核心头文件”

---
+++ include/layer.h
@@ -0,0 +1,9 @@
+#ifndef __LAYER__H__
+#define __LAYER__H__
+namespace gmap {
+    //存储图层信息，包括图层用到的数据，渲染图层的风格
+    class Layer {
+
+    }
+}
+#endif 
---
+++ include/rule.h
@@ -0,0 +1,9 @@
+#ifndef __RULE__H__
+#define __RULE__H__
+namespace gmap {
+    // the rule to render map ! 
+    class Rule {
+         
+    }
+}
+#endif
