3a8cc0019c12b3026aa24410a342c32cae836f88
cb jing <jingchenbo@gmail.com>
2019-07-03 23:08:34 +0800

    "添加工程gn"

--- include/map.h
+++ include/map.h
@@ -4,14 +4,13 @@
 namespace gmap {
     //Map类负责需要渲染的图片的所有信息的维护，比如地图的范围，分辨率。并且渲染图片
     class Map{
-        public:
-            Map();
-            //初始化地图
-            Map(int width, int hight);
-            void SetBounds(double xmin, double ymin, double xmax, double ymax);
-            void SetRes(double res);
-            bool Render
-
+    public:
+        Map();
+        //初始化地图
+        Map(int width, int hight);
+        void SetBounds(double xmin, double ymin, double xmax, double ymax);
+        void SetRes(double res);
+        bool Render();
     };
 }
 #endif
